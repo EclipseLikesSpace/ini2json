7f77f88f6d176236b3a552f5180e14f6c0772a65
Just-a-Unity-Dev <rnmangunay@addu.edu.ph>
2022-02-07 14:20:30 +0800

    file system

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ import parse from './modules/parser.js';
 const options = Yargs(process.argv.slice(2))
  .usage("Usage: -file <file location>")
  .option("file", { alias: "f", describe: "The file to convert", type: "string", demandOption: true })
- .option("outDirectory", { alias: "outdoc", describe: "Output location", type: "string", demandOption: false })
+ .option("outDirectory", { alias: "outdir", describe: "Output location", type: "string", demandOption: true })
  .option("viceversa", { alias: "vv", describe: "Should it convert .json to .ini instead of .ini to json?", type: "string", demandOption: false })
  .argv;
 
 console.log(chalk.blue("Converting file..."));
-fs.readFile(options.file, 'utf8', (err, data) => {
+fs.readFile('./' + options.file, 'utf8', (err, data) => {
     if (err) {
         console.log(chalk.red("Error: " + err));
         return;
@@ -21,7 +21,17 @@ fs.readFile(options.file, 'utf8', (err, data) => {
 
     let outLocation = options.outLocation;
     const lines = data.split('\n');
-    const convertedData = parse(lines);
-    if (!convertedData[1] == 200) { console.log(chalk.red("Parser error!" + convertedData[0])) }
-    console.log(chalk.green("File converted successfully!"));
+    const convertedData = JSON.stringify(parse(lines));
+    if (convertedData[1] == 500) { console.log(chalk.red("Parser error! Log: " + convertedData[0])) }
+    if (convertedData[1] == 200) {
+        console.log(chalk.green("File converted successfully!"));
+        console.log(chalk.blue("Writing to file..."));
+        fs.writeFile('./examples/test.json', toString(convertedData[0]), function(err){
+            if (err){
+                console.log(chalk.red("File Write Error: " + err));
+                return;
+            }
+            console.log(chalk.green("File written successfully!"));
+        });
+    }
 });
--- modules/parser.js
+++ modules/parser.js
@@ -1,23 +1,23 @@
 export default function parse(lines) {
-    let convertedData = JSON.stringify(`{}`);
-    console.log(lines.length)
+    let convertedData = {};
+    console.log(lines.length - 1);
 
     try {
         for(var i = 0; i < lines.length - 1; i++){
+            console.log('Iteration: ' + i)
             // Line is a comment, therefore should be ignored by the parser
-            if (lines[i].startsWith(';')){ console.log('Comment found, skipping'); continue; }
+            if (lines[i].startsWith(';') || lines[i].startsWith('#')){ console.log('Comment found, skipping'); }
             // Line is a section, therefore should be a new object
             if (lines[i].startsWith('[')){
                 console.log("Header found, parsing...");
                 const raw_line = lines[i]
-                const regex = /[\[\]']+/g
                 
-                const replace = raw_line.replace(regex, '')
+                const replace = raw_line.replace(/[\[\]']+/g, '');
     
-                convertedData[replace] = {}
-                continue;
+                convertedData[toString(replace)] = {init: "test"};
             }
         }
+
         return [convertedData, 200];
     } catch(err) {
         return [err, 500];
